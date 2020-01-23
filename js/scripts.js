81f44fd49631a1e72872cd50db71f2cb0c5759a6
Drake Wilcox and Matt Taylor <drake@>
2020-01-23 15:20:30 -0800

    add functions for when a word starts with q and u.

--- js/scripts.js
+++ js/scripts.js
@@ -2,33 +2,47 @@
 var sentence = []
 
 // sentence.charAt(0);
-console.log(pigLatin);
+// console.log(pigLatin);
+
 var pigLatin = function(sentence) {
+  
   if (sentence[0] === "a" || sentence[0] === "e" || sentence[0] === "i"|| sentence[0] === "o"|| sentence[0] === "u") {
     return (sentence+"way");
-  } else if (sentence[0]=== "p"){
-      // sentence.slice(0,1);
+  }  
+  else if (sentence[0] === "q" && sentence[1] === "u"){
+    return (sentence + (sentence.splice(0,2) + "way"));
+  
+  }else if (sentence[1] === "q" && sentence[2] === "u"){
+    return (sentence + (sentence.splice(0,3) + "way"));
+  }
+
+  else if (sentence[1] !== "a" && sentence[1] !== "e" && sentence[1] !== "i" && sentence[1] !== "o" && sentence[1] !== "u"){
+      return (sentence + (sentence.splice(0,2)+"way"));
+    }
+
+   else if (sentence[0] !== "a" && sentence[0] !== "e" && sentence[0] !== "i" && sentence[0] !== "o" && sentence[0] !== "u" && sentence[0] === "y"){
       return (sentence + (sentence.shift(0,1)+"way"));
       
+     
+     
   } else  {
     alert("False");
     return false;
+   
   }
+ 
 };
-
-// console.log(sentence);
-
 
 // User Logic
 $(document).ready(function() {
   $("form#input").submit(function(event) {
     event.preventDefault();
     var sentence = $("input#sentenceInput").val().split("");
-    console.log(sentence)
+    
     var result = pigLatin(sentence);
 
-
-
+    
+// console.log(result);
     $("#result").text(result);
   })
 })
